24a2c460015a364f0abdbb0068553dc7f14bb22b
YooranKim <yuranm@naver.com>
2024-01-20 18:25:51 -0800

    Spread all data

--- src/weather.js
+++ src/weather.js
@@ -1,33 +1,76 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faWind, faWater,faDroplet} from '@fortawesome/free-solid-svg-icons';
 
 const Weather = () => {
-    // const [weather, setWeather] = useState([]);
-
+    const [location, setLocation] = useState({});
+    const [current, setCurrent] = useState({});
+    const [condition, setCondition] = useState({});
+    const [forecast, setForecast] = useState([])
     useEffect(()=>{
         const fetch = async() =>{
             try{
-                const res = await axios.get(
-                    "http://api.weatherapi.com/v1/current.json?key=e6494ccd0346472e95063525241801&q=Vancouver&aqi=no"
-                );
-                sessionStorage.setItem("weather",JSON.stringify(res.data))
+                const res = await axios.get("http://api.weatherapi.com/v1/forecast.json?key=e6494ccd0346472e95063525241801&q=Vancouver&days=7&aqi=no&alerts=no");
+                setLocation(res.data.location);
+                setCurrent(res.data.current);
+                setCondition(res.data.current.condition);
+                setForecast(res.data.forecast.forecastday)
             }catch(e){
                 console.log(e);
             }
         };
         fetch();
     },[]);
-
-    let weatherSession = JSON.parse(sessionStorage.getItem("weather"))
-    let location = weatherSession.location;
-    let current = weatherSession.current;
+    const date = new Date();
+    const today = `${date.getFullYear()}.${date.getMonth()+1}.${date.getDay()}`;
+    console.log(forecast);
     return (
-        <>
+        <div style={{backgroundColor: "gray"}}>
             <p>Hello</p>
-       
+            {location.name}
             {location.region}
             {current.temp_c}
-        </>
+            {/* container */}
+            <div>
+                {/* name temp icon */}
+                <div style={{border:"1px solid red"}}>
+                    <div>
+                        <div>{location.region}</div>
+                        <div>{today}</div>
+                        <div>{current.temp_c}</div>
+                        <div>{condition.text}</div>
+                    </div>
+                    <div>
+                        <img src={condition.icon} alt=''/>
+                    </div>
+                </div>
+
+                {/* wind */}
+                <div>
+                    <div>
+                        <FontAwesomeIcon icon={faWind} />
+                        <p>{current.wind_mph}mph</p>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faWater} />
+                        <p>{current.precip_mm}mm</p>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faDroplet} />
+                        <p>{current.humidity}</p>
+                    </div>
+                </div>
+
+                {/* forecast */}
+                <div>
+                    {forecast.map((day,idx)=>{
+                        return <div> <p>Mon</p><img src={day.day.condition.icon}></img><p key={idx}>{day.day.avgtemp_c}&deg;C</p></div>
+                    })}
+                </div>
+            </div>
+
+        </div>
     );
     
 }
